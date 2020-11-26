c38c1e45dd2d312d99b6c948e3d86fe44d17bec5
saimcagirici <cagirici@live.com>
2020-11-25 20:09:36 -0500

    h

--- src/test/java/day3/UtilityPractice.java
+++ src/test/java/day3/UtilityPractice.java
@@ -9,18 +9,26 @@ public class UtilityPractice {
 
     public static void main(String[] args) throws SQLException {
 
+        // test out all the methods you have created so far for practice
         DB_Utility.createConnection();
+
         ResultSet jobRS = DB_Utility.runQuery("SELECT * FROM JOBS");
 
+        /**
+         * Get the row count of the ResultSet
+         * move the pointer to the last row and get the row number
+         */
 
-        int rowCount = DB_Utility.getRowCount();
+        int rowCount =  DB_Utility.getRowCount() ;
         System.out.println("rowCount = " + rowCount);
 
-        int colCount = DB_Utility.getColumnCount();
+        int colCount = DB_Utility.getColumnCount() ;
         System.out.println("colCount = " + colCount);
 
 
         DB_Utility.destroy();
 
     }
+
+
 }
--- src/test/java/utility/DB_Utility.java
+++ src/test/java/utility/DB_Utility.java
@@ -15,71 +15,89 @@ public class DB_Utility {
         String password = "hr";
 
         try {
-            conn = DriverManager.getConnection(connectionStr, username, password);
+            conn = DriverManager.getConnection(connectionStr,username,password) ;
             System.out.println("CONNECTION SUCCESSFUL !! ");
         } catch (SQLException e) {
-            System.out.println("CONNECTION HAS FAILED!!!!! " + e.getMessage());
-            ;
+            System.out.println("CONNECTION HAS FAILED !!! " +  e.getMessage() );
         }
 
     }
+    // Create a method called runQuery that accept a SQL Query
+    // and return ResultSet Object
+    public static ResultSet runQuery(String query){
 
-    public static ResultSet runQuery(String query) {
-
-        ResultSet rs = null;
-
+//        ResultSet rs  = null;
+        // reusing the connection built from previous method
         try {
             stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            rs = stmnt.executeQuery(query);
+            rs = stmnt.executeQuery(query) ;
+
         } catch (SQLException e) {
             System.out.println("Error while getting resultset " + e.getMessage());
         }
 
-        return rs;
+        return rs ;
 
     }
+    // create a method to clean up all the connection statemnet and resultset
+    public static void destroy(){
 
-    public static void destroy() {
         try {
+
             rs.close();
             stmnt.close();
             conn.close();
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
+
     }
 
-    public static int getRowCount() {
-        int rowCount = 0;
+    /**
+     * Count how many row we have
+     * @return the row count of the resultset we got
+     */
+    public static int getRowCount(){
+
+        int rowCount = 0 ;
+
         try {
             rs.last();
-            rowCount = rs.getRow();
+            rowCount = rs.getRow() ;
 
+            // move the cursor back to beforeFirst location to avoid accident
             rs.beforeFirst();
 
         } catch (SQLException e) {
 
-            System.out.println("ERROR WHILE GETTING ROW COUNt " + e.getMessage());
+            System.out.println("ERROR WHILE GETTING ROW COUNT "  + e.getMessage() );
         }
-        return rowCount;
+
+        return rowCount ;
 
     }
 
+    /**
+     * Get the column count
+     * @return count of column the result set have
+     */
     public static int getColumnCount(){
 
-        int columnCount  = 0;
-
+        int columnCount = 0 ;
 
         try {
-
-            ResultSetMetaData rsmd = rs.getMetaData();
+            ResultSetMetaData rsmd = rs.getMetaData() ;
             columnCount = rsmd.getColumnCount();
 
         } catch (SQLException e) {
-            System.out.println("ERROR WHILE COUNTING THE COLUMNS "+ e.getMessage());
+            System.out.println("ERROR WHILE COUNTING THE COLUMNS " + e.getMessage() );
         }
-        return columnCount;
+
+        return columnCount ;
     }
+
+
 
 }
